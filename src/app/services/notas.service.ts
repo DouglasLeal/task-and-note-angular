99595c8d5e76a088343438f776c49d56988b8698
Douglas Leal <douglas.leal@live.com>
2023-03-07 17:56:55 -0300

    Nota - service crud completo

--- src/app/services/notas.service.ts
+++ src/app/services/notas.service.ts
@@ -3,10 +3,32 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import Nota from '../models/Nota';
+
 @Injectable({
   providedIn: 'root'
 })
 export class NotasService {
 
-  
+  private readonly url: string = `${environment.apiUrl}/notas`;
+
+  constructor(private http: HttpClient) { }
+
+  get(): Observable<Nota[]>{
+    return this.http.get<Nota[]>(this.url);
+  }
+
+  post(nota: Nota): Observable<Nota> {
+    return this.http.post<Nota>(this.url, nota);
+  }
+
+  put(nota: Nota): Observable<Nota> {
+    const url = `${this.url}/${nota._id}`
+    return this.http.put<Nota>(url, nota);
+  }
+
+  delete(id: string): Observable<Nota> {
+    const url = `${this.url}/${id}`
+    return this.http.delete<Nota>(url);
+  }
 }
