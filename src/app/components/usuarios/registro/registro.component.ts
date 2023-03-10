bd92cc1f2bba7b3d88aad0d75b9aa3b95e7bf64b
Douglas Leal <douglas.leal@live.com>
2023-03-10 11:48:31 -0300

    Usuario - registro funcional

--- src/app/components/usuarios/registro/registro.component.ts
+++ src/app/components/usuarios/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Usuario from 'src/app/models/Usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.scss']
 })
 export class RegistroComponent {
+  public usuario: Usuario = new Usuario({});
+  public confirmaSenha: string = "";
 
+  constructor(private service: UsuarioService){}
+
+  registrarUsuario(){
+    this.service.registrar(this.usuario).subscribe(result => {
+      console.log(result);
+    });
+  }
 }
--- src/app/components/usuarios/usuarios.module.ts
+++ src/app/components/usuarios/usuarios.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { UsuariosRoutingModule } from './usuarios-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { RegistroComponent } from './registro/registro.component';
   ],
   imports: [
     CommonModule,
-    UsuariosRoutingModule
+    UsuariosRoutingModule,
+    FormsModule
   ]
 })
 export class UsuariosModule { }
